55fddc12fa71c50f077ac161b9ca363c0777cd54
iamEvgen <evgen.alexa@gmail.com>
2022-03-16 16:27:43 +0300

    Etch a sketch is ready v1.0

--- javascript.js
+++ javascript.js
@@ -6,6 +6,8 @@ field.style.gap = '1px';
 field.style.background = 'black';
 
 const colorBlack = document.getElementById('colorBlack');
+const colorRainbow = document.getElementById('colorRainbow');
+const colorRGB = document.getElementById('colorRGB');
 
 const size16 = document.getElementById('size16');
 const size32 = document.getElementById('size32');
@@ -14,16 +16,49 @@ const size64 = document.getElementById('size64');
 const erase = document.getElementById('cmdErase');
 
 let cells = [];
-let color = 'black';
-let rainbowMode = false;
+let colorMode = 'black'; // 'black' || 'rgb' || 'rainbow'
+
+function setButtonColors() {
+    size16.style.background = 'white';
+    size32.style.background = 'white';
+    size64.style.background = 'white';
+    let selected = 'rgb(150,150,200)';
+    if (fieldSize === 16) {
+        size16.style.background = selected;
+    } else if (fieldSize === 32) {
+        size32.style.background = selected;
+    } else {
+        size64.style.background = selected;
+    }
+
+    colorBlack.style.background = 'white';
+    colorRainbow.style.background = 'white';
+    if (colorMode === 'black') {
+        colorBlack.style.background = selected;
+    } else if (colorMode === 'rainbow') {
+        colorRainbow.style.background = selected;
+    }
+}
 
 function setFieldSize(size) {
     fieldSize = size;
+    setButtonColors();
     drawField();
+}
+
+function generateRainbowColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
 function colorizeCell(cell, event) {
     if (event.buttons) {
+        if (colorMode === 'rainbow') {
+            color = generateRainbowColor();
+        } else if (colorMode === 'black') {
+            color = 'black';
+        } else if (colorMode === 'rgb') {
+            color = colorRGB.value;
+        }
         cell.style.background = color;
     }
 }
@@ -68,7 +103,7 @@ function drawField() {
     }
     addEventListener();
 }
-
+setButtonColors()
 drawField();
 
 size16.addEventListener('click', setFieldSize.bind(null, 16));
@@ -76,4 +111,7 @@ size32.addEventListener('click', setFieldSize.bind(null, 32));
 size64.addEventListener('click', setFieldSize.bind(null, 64));
 
 erase.addEventListener('click', drawField);
-colorBlack.addEventListener('click', () => {color = 'black'; rainbowMode = false;})
+
+colorBlack.addEventListener('click', () => {colorMode = 'black', setButtonColors()});
+colorRainbow.addEventListener('click', () => {colorMode = 'rainbow', setButtonColors()});
+colorRGB.addEventListener('click', () => {colorMode = 'rgb', setButtonColors()})
